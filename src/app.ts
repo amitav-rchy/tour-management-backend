544a7b13214f328bfb94e8e1b7914b7ca8afdc9e
Amitav <amitavrchy01@gmail.com>
2025-10-03 12:20:20 +0600

    latest push

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express, { type Request, type Response } from "express";
 import cors from "cors"
-import { UserRoutes } from "./app/modules/user/user.route";
+import { router } from "./app/routes";
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use("/api/v1/user", UserRoutes)
+app.use("/api/v1", router)
 
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json("Welcome to tour management app");
